7c9fcd7a7975881b110c500a1b0a31fdc7a099a2
Eduardo Tomaz <eduardotoomaz@outlook.com>
2025-03-20 12:15:03 -0300

    Começo do curso Java10x

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,23 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+         * Dados primitivos - int, double, float, char, boolean, short.
+         * Objetivo da aula: Criar um ninja - Naruto -
+         */
+
+        int idade = 17; // Valor maximo: 2 147 483 647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 999999L ; // Valor maximo: 9,223,372,036,854,775,807
+
+        System.out.println(idade); // Comando para mostrar para o usuario
+        System.out.println(saldoBancario);
+        System.out.println(altura);
+        System.out.println("Saldo Bancario é = " + saldoBancario);
+        System.out.println("Minha idade é: " + idade);
+    }
+
+}
